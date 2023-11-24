1818b0c7c6909d8331cc1958752ff04527a52029
Kozo Nishida <kozo.nishida@gmail.com>
2023-11-24 20:27:55 +0900

    Add "Display progress and status" to cys upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,10 +60,12 @@ st.title("msdial2cytoscape")
 cys_file = st.file_uploader("Upload a Cytoscape session [cys] file", type="cys")
 
 if cys_file is not None:
-    with open(cys_file.name, 'wb') as f:
-        f.write(cys_file.getbuffer())
-    p4c.sandbox_send_to(cys_file.name, base_url='http://cytoscape-desktop:1234/v1')
-    p4c.open_session(cys_file.name, base_url='http://cytoscape-desktop:1234/v1')
+    with st.spinner('Loading Cytoscape session [cys] file...'):
+        with open(cys_file.name, 'wb') as f:
+            f.write(cys_file.getbuffer())
+        p4c.sandbox_send_to(cys_file.name, base_url='http://cytoscape-desktop:1234/v1')
+        p4c.open_session(cys_file.name, base_url='http://cytoscape-desktop:1234/v1')
+    st.success('Done! Please check if your network is visible on http://localhost:6080/vnc_auto.html')
 else:
     st.write("Upload a Cytoscape session [cys] to get started.")
 
